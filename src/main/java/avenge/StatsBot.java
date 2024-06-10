b9fee1927122a1de67b2090d3ec99231576d98b7
Dmytro <dimac2009@gmail.com>
2024-06-10 13:21:31 +0300

    Update StatsBot.java

--- src/main/java/avenge/StatsBot.java
+++ src/main/java/avenge/StatsBot.java
@@ -7,21 +7,39 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class StatsBot extends TelegramLongPollingBot {
     @Override
-    public String getBotUsername() {
-        return "@StatsMCoC_bot";
-    }
-
-    @Override
-    public String getBotToken() {
-        return "7071295461:AAEahEf_EzkOwU0IwoRXFibLz25Ch9PcP4U";
-    }
-
-    @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
+            String username = update.getMessage().getFrom().getUserName();
+            String firstName = update.getMessage().getFrom().getFirstName();
+
             if (messageText.equals("/start")) {
-                SendMessage message = StartCommand.execute(update);
+                // Проверка, существует ли пользователь в базе данных
+                if (!mongoDBService.userExists(username)) {
+                    mongoDBService.saveUserData(username, firstName);
+                }
+
+                // Создание кнопки "Start App" с использованием WebAppInfo
+                InlineKeyboardButton startAppButton = new InlineKeyboardButton();
+                startAppButton.setText("Start App");
+                WebAppInfo webAppInfo = new WebAppInfo("https://avengerdima.github.io/StatsMCoC_bot/");
+                startAppButton.setWebApp(webAppInfo);
+
+                List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
+                keyboardButtonsRow.add(startAppButton);
+
+                List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+                rowList.add(keyboardButtonsRow);
+
+                InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+                inlineKeyboardMarkup.setKeyboard(rowList);
+
+                // Отправка сообщения с кнопкой
+                SendMessage message = new SendMessage();
+                message.setChatId(update.getMessage().getChatId().toString());
+                message.setText("Приветствую, Призыватель!");
+                message.setReplyMarkup(inlineKeyboardMarkup);
+
                 try {
                     execute(message);
                 } catch (Exception e) {
@@ -29,5 +47,4 @@ public class StatsBot extends TelegramLongPollingBot {
                 }
             }
         }
-    }
 }
